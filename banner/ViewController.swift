3940af863e8d4c567c34149d7d40595a210c30f4
Muhammad Aamir <>
2020-08-28 23:17:32 +0500

    auto correct liting

--- banner/ViewController.swift
+++ banner/ViewController.swift
@@ -21,15 +21,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         setupDefault()
     }
-    
+
     private func setupDefault() {
         greenTapped()
     }
-    
+
     @IBAction func autoDismiss(_ sender: UISwitch) {
         BannerConfiguration.shared.autoDismiss = sender.isOn
     }
-    
+
     @IBAction private func blueTapped() {
         BannerConfiguration.shared.bannerType = .info
         blue.backgroundColor = #colorLiteral(red: 0.2329987586, green: 0.6000669003, blue: 0.8483656645, alpha: 1)
@@ -45,7 +45,7 @@ class ViewController: UIViewController {
         orange.backgroundColor = #colorLiteral(red: 0.9294117647, green: 0.6196078431, blue: 0.2156862745, alpha: 0.5)
         red.backgroundColor = #colorLiteral(red: 0.8862745098, green: 0.3176470588, blue: 0.2823529412, alpha: 0.5)
     }
-    
+
     @IBAction private func orangeTapped() {
         BannerConfiguration.shared.bannerType = .warning
         blue.backgroundColor = #colorLiteral(red: 0.231372549, green: 0.6, blue: 0.8470588235, alpha: 0.5)
@@ -53,7 +53,7 @@ class ViewController: UIViewController {
         orange.backgroundColor = #colorLiteral(red: 0.9294117647, green: 0.6196078431, blue: 0.2156862745, alpha: 1)
         red.backgroundColor = #colorLiteral(red: 0.8862745098, green: 0.3176470588, blue: 0.2823529412, alpha: 0.5)
     }
-    
+
     @IBAction private func redTapped() {
         BannerConfiguration.shared.bannerType = .danger
         blue.backgroundColor = #colorLiteral(red: 0.231372549, green: 0.6, blue: 0.8470588235, alpha: 0.5)
@@ -61,7 +61,7 @@ class ViewController: UIViewController {
         orange.backgroundColor = #colorLiteral(red: 0.9294117647, green: 0.6196078431, blue: 0.2156862745, alpha: 0.5)
         red.backgroundColor = #colorLiteral(red: 0.8862745098, green: 0.3176470588, blue: 0.2823529412, alpha: 1)
     }
-    
+
     @IBAction private func showBanner() {
         presentBanner(message: "Custom message")
     }
